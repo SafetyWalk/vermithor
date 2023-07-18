8d1e383d2eb7693435cad79ad455c3f4cb80abe3
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-07-18 12:34:06 +0700

    remove unused line

--- lib/stores/theme/theme_store.dart
+++ lib/stores/theme/theme_store.dart
@@ -8,7 +8,7 @@ part 'theme_store.g.dart';
 class ThemeStore = _ThemeStore with _$ThemeStore;
 
 abstract class _ThemeStore with Store {
-  final String TAG = "_ThemeStore";
+  // final String TAG = "_ThemeStore";
 
   // repository instance
   final Repository _repository;
@@ -46,8 +46,8 @@ abstract class _ThemeStore with Store {
       MediaQuery.platformBrightnessOf(context) == Brightness.dark;
 
   // dispose:-------------------------------------------------------------------
-  @override
-  dispose() {
+  // @override
+  // dispose() {
 
-  }
+  // }
 }
