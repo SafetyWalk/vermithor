d9cb69626fadb036222d243cbaade36d4693bdf6
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-07-22 15:34:23 +0700

    insert api key on iOS

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,10 +1,12 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import <GoogleMaps/GoogleMaps.h> // Add this line
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"PUT_THE_GOOGLE_MAPS_API_KEY"]; // Replace with API key
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
