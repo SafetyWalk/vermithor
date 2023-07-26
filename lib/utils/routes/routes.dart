931daa68ba0ee19da664d6d19ed56eb68da95635
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-07-26 10:29:30 +0700

    adding map & post route

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -1,5 +1,7 @@
 import 'package:safewalk/ui/home/home.dart';
 import 'package:safewalk/ui/login/login.dart';
+import 'package:safewalk/ui/maps/maps.dart';
+import 'package:safewalk/ui/post/post.dart';
 import 'package:safewalk/ui/splash/splash.dart';
 import 'package:flutter/material.dart';
 
@@ -10,11 +12,15 @@ class Routes {
   static const String splash = '/splash';
   static const String login = '/login';
   static const String home = '/home';
+  static const String map = '/map';
+  static const String post = '/post';
 
   static final routes = <String, WidgetBuilder>{
     splash: (BuildContext context) => SplashScreen(),
     login: (BuildContext context) => LoginScreen(),
     home: (BuildContext context) => HomeScreen(),
+    map : (BuildContext context) => MapsScreen(),
+    post: (BuildContext context) => PostScreen(),
   };
 }
 
