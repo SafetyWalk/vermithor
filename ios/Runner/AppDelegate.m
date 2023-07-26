3476855586fb91408ae574e9c2c971fed7c840d3
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-07-26 10:30:11 +0700

    added maps api key

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GMSServices provideAPIKey:@"PUT_THE_GOOGLE_MAPS_API_KEY"]; // Replace with API key
+  [GMSServices provideAPIKey:@"AIzaSyAneuo8D6mL2CRNUu5u2E4S3FxDHmob1ZU"]; // Replace with API key
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
