b08bd65cd0b7857ec387539926f4fc50fb726cab
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-08-15 10:34:57 +0700

    styling dashboard🎨

--- lib/ui/home/dashboard.dart
+++ lib/ui/home/dashboard.dart
@@ -1,58 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:safewalk/constants/colors.dart';
 import 'package:safewalk/utils/device/device_utils.dart';
 import 'package:safewalk/utils/routes/routes.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    )..repeat(reverse: true);
+
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 1.1).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () {
-              // context.read<AuthCubit>().logout();
-            },
-          )
-        ],
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: DeviceUtils.getScaledHeight(context, 0.3),
-            ),
-            Text('Dashboard'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(Routes.library);
-              },
-              child: Text('Go to Library'),
-            ),  
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(Routes.storage);
-              },
-              child: Text('Go to Storage'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(Routes.map);
-              },
-              child: Text(
-                "Go to Maps"
+      appBar: AppBar(),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pushNamed(Routes.navigator);
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: DeviceUtils.getScaledHeight(context, 0.2),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(Routes.distanceMap);
-              },
-              child: Text(
-                "Go to Distance Maps"
+              AnimatedBuilder(
+                animation: _scaleAnimation,
+                builder: (context, child) {
+                  return Transform.scale(
+                    scale: _scaleAnimation.value,
+                    child: Column(
+                      children: [
+                        Container(
+                          constraints:
+                              BoxConstraints(maxWidth: 350, maxHeight: 350),
+                          child: Image.asset(
+                            'assets/icons/ic_safewalk.png',
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        ShaderMask(
+                          shaderCallback: (Rect bounds) {
+                            return LinearGradient(
+                              colors: [
+                                AppColors.primaryGradientDarkTopLeft,
+                                AppColors.primaryGradientDarkBottomRight
+                              ],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                            ).createShader(bounds);
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 16.0),
+                            child: Text(
+                              "SafeWalk",
+                              style: TextStyle(
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors
+                                    .white, // The color of the text within the gradient
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text(
+                            'Tap to Start',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors
+                                  .white, // The color of the text within the gradient
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
