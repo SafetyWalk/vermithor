f6d39c1eb358642dc8f5cbd928db2e35c59c8078
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-08-16 09:10:08 +0700

    register new endpoints

--- lib/data/network/constants/endpoints.dart
+++ lib/data/network/constants/endpoints.dart
@@ -7,7 +7,8 @@ class Endpoints {
   static final String postUrl = "https://jsonplaceholder.typicode.com";
 
   // base url
-  static final String baseUrl = Environment.asclepiusURL;
+  // static final String baseUrl = Environment.asclepiusURL;
+  static final String baseUrl = "https://safewalk-testing-ae.et.r.appspot.com/api/v1";
 
   // receiveTimeout
   static const int receiveTimeout = 15000;
@@ -18,8 +19,16 @@ class Endpoints {
   // booking endpoints
   static final String getPosts = postUrl + "/posts";
 
-  // login endpoints
+  // authentication endpoints
   static final String auth = baseUrl + "/authentication";
-  static final String loginRegular = auth + "/manual-user";
-  static final String loginGoogle = auth + "/google-user";
+  static final String loginManual = auth + "/manual-user/login";
+  static final String loginGoogle = auth + "/google-user/login";
+  static final String registerManual = auth + "/manual-user";
+  static final String registerGoogle = auth + "/google-user";
+  static final String editPassword = auth + "/manual-user/edit/password/";
+
+  // profile endpoints
+  static final String profile = baseUrl + "/profile";
+  static final String profileManual = profile + "/manual-user";
+  static final String profileGoogle = profile + "/google-user";
 }
