20b6d44536a12fc612a62c4c58bce751ac38825b
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-08-17 11:01:22 +0700

    modify parameter input

--- lib/data/network/apis/auth/register/register_api.dart
+++ lib/data/network/apis/auth/register/register_api.dart
@@ -11,16 +11,24 @@ class RegisterApi {
   // injecting dio instance
   RegisterApi(this._dioClient);
 
-  Future<dynamic> registerManual(dynamic data) async {
+  Future<dynamic> registerManual(
+    String username,
+    String email,
+    String, password,
+    String first_name,
+    String last_name,
+    String mobile_number,
+    String photo_url,
+  ) async {
     try {
       final res = await _dioClient.post(Endpoints.loginManual, data: {
-          "username": data["username"],
-          "email": data["email"],
-          "password": data["password"],
-          "first_name": data["first_name"],
-          "last_name": data["last_name"],
-          "mobile_number": data["mobile_number"],
-          "photo_url": data["photo_url"]
+        "username": username,
+        "email": email,
+        "password": password,
+        "first_name": first_name,
+        "last_name": last_name,
+        "mobile_number": mobile_number,
+        "photo_url": photo_url
       });
       return res;
     } catch (e) {
@@ -28,15 +36,22 @@ class RegisterApi {
     }
   }
 
-  Future<dynamic> registerGoogle(dynamic data) async {
+  Future<dynamic> registerGoogle(
+    String google_uid,
+    String email,
+    String password,
+    String name,
+    String mobile_number,
+    String photo_url,
+  ) async {
     try {
       final res = await _dioClient.post(Endpoints.loginGoogle, data: {
-          "google_uid": data["google_uid"],
-          "email": data["email"],
-          "password": data["password"],
-          "name": data["name"],
-          "mobile_number": data["mobile_number"],
-          "photo_url": data["photo_url"]
+        "google_uid": google_uid,
+        "email": email,
+        "password": password,
+        "name": name,
+        "mobile_number": mobile_number,
+        "photo_url": photo_url
       });
       return res;
     } catch (e) {
